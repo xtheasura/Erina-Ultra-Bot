a35b6e225fd0a685acbdd39f5b8b8728945b2cac
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-10-28 18:28:31 +0530

    Update __init__.py

--- Erina/__init__.py
+++ Erina/__init__.py
@@ -27,7 +27,7 @@ BOT_ID = x.id
 
 x = None
 
-PTB = Application.builder().token(TOKEN).defaults(defaults).concurrent_updates(True).build()
+PTB = Application.builder().token(TOKEN).Defaults(defaults).concurrent_updates(True).build()
 
 
 MOD_LOAD = []
