ab0ef8525bc2fcab550acfe6a44c4afbbabd7fac
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-11-05 07:51:25 -0700

    Update __main__.py

--- Erina/__main__.py
+++ Erina/__main__.py
@@ -8,6 +8,22 @@ from Erina import BOT_USERNAME
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup 
 import asyncio
 
+from math import ceil
+
+from pyrogram.types import InlineKeyboardButton
+
+from Erina import MOD_LOAD, MOD_NOLOAD
+
+class EqInlineKeyboardButton(InlineKeyboardButton):
+    def __eq__(self, other):
+        return self.text == other.text
+
+    def __lt__(self, other):
+        return self.text < other.text
+
+    def __gt__(self, other):
+        return self.text > other.text
+
 PM_ERINA_TEXT = """
 **𝑆𝑢𝑝 !!! 𝐼 𝐴𝑚 𝐸𝑟𝑖𝑛𝑎, 𝑁𝑖𝑐𝑒 𝑇𝑜 𝑀𝑒𝑒𝑡 𝑌𝑜𝑢 !!**
 𝐼 𝑎𝑚 𝐴𝑛𝑑 𝑎𝑑𝑣𝑎𝑛𝑐𝑒𝑑 𝐵𝑜𝑡
@@ -47,6 +63,112 @@ async def miostart(_, message):
       reply_markup=START_KEYBOARD
     ) 
 
+def paginate_modules(page_n, module_dict, prefix, chat=None):
+    if not chat:
+        modules = sorted(
+            [
+                EqInlineKeyboardButton(
+                    x.__MODULE__,
+                    callback_data="{}_module({})".format(
+                        prefix, x.__MODULE__.replace(" ", "_").lower()
+                    ),
+                )
+                for x in module_dict.values()
+            ]
+        )
+    else:
+        modules = sorted(
+            [
+                EqInlineKeyboardButton(
+                    x.__MODULE__,
+                    callback_data="{}_module({},{})".format(
+                        prefix, chat, x.__MODULE__.replace(" ", "_").lower()
+                    ),
+                )
+                for x in module_dict.values()
+            ]
+        )
+
+    pairs = list(zip(modules[::3], modules[1::3], modules[2::3]))
+    i = 0
+    for m in pairs:
+        for _ in m:
+            i += 1
+    if len(modules) - i == 1:
+        pairs.append((modules[-1],))
+    elif len(modules) - i == 2:
+        pairs.append(
+            (
+                modules[-2],
+                modules[-1],
+            )
+        )
+
+    COLUMN_SIZE = 4
+
+    max_num_pages = ceil(len(pairs) / COLUMN_SIZE)
+    modulo_page = page_n % max_num_pages
+
+    # can only have a certain amount of buttons side by side
+    if len(pairs) > COLUMN_SIZE:
+        pairs = pairs[
+                modulo_page * COLUMN_SIZE: COLUMN_SIZE * (modulo_page + 1)
+                ] + [
+                    (
+                        EqInlineKeyboardButton(
+                            "❮",
+                            callback_data="{}_prev({})".format(prefix,
+                                                               modulo_page),
+                        ),
+                        EqInlineKeyboardButton(
+                            "Back",
+                            callback_data="{}_home({})".format(prefix,
+                                                               modulo_page),
+                        ),
+                        EqInlineKeyboardButton(
+                            "❯",
+                            callback_data="{}_next({})".format(prefix,
+                                                               modulo_page),
+                        ),
+                    )
+                ]
+
+    return pairs
+
+
+def is_module_loaded(name):
+    return (not MOD_LOAD or name in MOD_LOAD) and name not in MOD_NOLOAD  
+  
+BOT_NAME = "Erina"  
+  
+async def help_parser(name, keyboard=None):
+    if not keyboard:
+        keyboard = InlineKeyboardMarkup(paginate_modules(0, HELPABLE, "help"))
+    return (
+        """Hello {first_name}, My name is Erina.
+I'm a group management bot with some useful features.
+You can choose an option below, by clicking a button.
+Also you can ask anything in Support Group.
+""".format(
+            first_name=name,
+            bot_name=BOT_NAME,
+        ),
+        keyboard,
+    )
+  
+  
+@bot.on_callback_query(filters.regex("bot_commands"))
+async def commands_callbacc(_, CallbackQuery):
+    text, keyboard = await help_parser(CallbackQuery.from_user.mention)
+    await bot.send_message(
+        CallbackQuery.message.chat.id,
+        text=text,
+        reply_markup=keyboard,
+    )
+
+    await CallbackQuery.message.delete()
+  
+  
 FORMAT = "[INFO] %(message)s"
 
 if __name__ == "__main__":
