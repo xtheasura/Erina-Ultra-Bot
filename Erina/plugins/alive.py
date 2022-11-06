43c63c7ea92fe0c48e7ddc91d65b58d17e902059
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-11-06 04:16:43 -0800

    Update alive.py

--- Erina/plugins/alive.py
+++ Erina/plugins/alive.py
@@ -3,6 +3,6 @@ from pyrogram import filters
 from Erina import bot
 
 
-@bot.on_message(filters.command("alive", "."))
+@bot.on_message(filters.command("alive"))
 async def alive(bot, message):
-    await message.edit_text("`I'm alive` \n PTB: `v20.a0` \n Pyrogram: `2.0.4`")
+    await bot.send.message("`I'm alive` \n PTB: `v20.a0` \n Pyrogram: `2.0.4`")
